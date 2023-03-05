84ae4113833e14d22015583637ea5e6a47aadc50
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-03-05 15:43:45 +0400

    Add project files.

---
+++ Two-dimensional array/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Two_dimensional_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            //int[,] arr = new int[3, 3]
+            //    {
+            //          {5,9,4},
+            //          {3,5,6},
+            //          {2,1,9},
+            //    };
+            //int sum = 0;
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++)
+            //    {
+            //        if (i == j)
+            //        {
+            //            sum += arr[i, j];
+            //        }
+
+            //    }
+
+            //}
+
+            //Console.WriteLine(sum);
+
+            //Q2
+            //int[,] arr = new int[2, 2]
+            //{
+            //    {2,3 },
+            //    {5,4 }
+            //};
+            //int[,] ints = new int[2, 2]
+            //{
+            //    {9,11 },
+            //    {3,6 }
+            //};
+            //int[,] sum = new int[2, 2];
+
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int n = 0; n < arr.GetLength(1); n++)
+            //    {
+            //        sum[i, n] = arr[i, n] + ints[i, n];
+            //        Console.Write(sum[i, n] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+        }
+    }
+}
